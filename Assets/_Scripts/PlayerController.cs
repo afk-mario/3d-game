576a9689b1ea6266755944d99ae00e7f2012f0f5
Mario Carballo Zama <afk@ellugar.co>
2020-10-27 20:21:48 -0600

    feature: Simple Movement using new Input System

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour {
+    private Vector2 _inputVector;
+    private float speed = 0.1f;
+
+    void FixedUpdate () {
+        transform.position = new Vector3 (
+            transform.position.x + _inputVector.x * speed,
+            transform.position.y + _inputVector.y * speed,
+            transform.position.z
+        );
+    }
+
+    public void HandleInteract (InputAction.CallbackContext context) {
+        Debug.Log ("Player interact");
+    }
+
+    public void HandleMove (InputAction.CallbackContext context) {
+        _inputVector = context.ReadValue<Vector2> ();
+    }
+}
